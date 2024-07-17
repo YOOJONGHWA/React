1b84950b300d7ca9726a0edb6199b480e7889918
유종화 <sola201@naver.com>
2024-07-17 14:17:22 +0900

    연동확인 커밋

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React22222
         </a>
       </header>
     </div>
